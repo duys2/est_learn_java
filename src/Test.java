852078d76505b914e52ac0aecc26d300a9e1ac82
Ays <minigun5@naver.com>
2024-05-30 09:35:40 +0900

    init

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+	// 자바Doc
+
+	/**
+	@author ys
+	@apiNote  main method
+	**/
+    public static void main(String[] args) {
+		System.out.println("Hello World");
+		// System.out.println("sout");
+	}
+}
