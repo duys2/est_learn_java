3e85b0ec5194cc2271ad0bb28ad87772c418c94b
Ays <minigun5@naver.com>
2024-05-31 14:01:26 +0900

    [Feat] 배열 학습

---
+++ src/ArrayLearn.java
@@ -0,0 +1,20 @@
+public class ArrayLearn {
+  public static void main(String[] args) {
+    // int[] arr = new int[100]; // 크기가 100인 정수형 배열 arr
+
+    // System.out.println(arr);
+    // System.out.println(arr[0]);
+
+    // for (int i = 0; i < arr.length; i++) {
+    //   System.out.println(arr[i]);
+    // }
+
+    // String[] arr2 = new String[1000]; // 크기가 1000인 문자열 배열 arr2
+    //
+    // System.out.println(arr2);
+    // System.out.println(arr2[0]);
+
+
+
+  }
+}
