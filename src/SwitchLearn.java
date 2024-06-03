5a90df7f66e23b4b00b90aee9242e9ab696e7a59
Ays <minigun5@naver.com>
2024-06-03 16:00:58 +0900

    [Add] switch 문 학습

---
+++ src/SwitchLearn.java
@@ -0,0 +1,157 @@
+public class SwitchLearn {
+  public static void main(String[] args) {
+    // String day = "수요일";
+    //
+    // switch (day) {
+    //   case "월요일":
+    //     System.out.println("책 읽기");
+    //     break;
+    //   case "화요일":
+    //     System.out.println("공부하기");
+    //     break;
+    //   case "수요일":
+    //     System.out.println("운동하기");
+    //     break;
+    //   case "목요일":
+    //     System.out.println("고기 먹기");
+    //     break;
+    //   case "금요일":
+    //     System.out.println("게임하기");
+    //     break;
+    //   case "토요일":
+    //     System.out.println("알바가기");
+    //     break;
+    //   case "일요일":
+    //     System.out.println("산책하기");
+    //     break;
+    //   default:
+    //     System.out.println("알 수 없는 요일");
+    // }
+
+    // 문제: 변수 grade에 'A', 'B', 'C', 'D', 'F' 중 하나의 문자가 저장되어 있습니다. 각 등급에 따라 "Excellent", "Good",
+    // "Average", "Below Average", "Fail"을 출력하는 코드를 작성하세요.
+    // char grade = 'B';
+    //
+    // switch (grade) {
+    //   case 'A':
+    //     System.out.println("Excellent");
+    //     break;
+    //   case 'B':
+    //     System.out.println("Good");
+    //     break;
+    //   case 'C':
+    //     System.out.println("Average");
+    //     break;
+    //   case 'D':
+    //     System.out.println("Below Average");
+    //     break;
+    //   case 'E':
+    //     System.out.println();
+    //     break;
+    //   default:
+    //     System.out.println("제대로 된 성적을 입력하세요.");
+    // }char grade = 'B';
+    //
+    // switch (grade) {
+    //   case 'A':
+    //     System.out.println("Excellent");
+    //     break;
+    //   case 'B':
+    //     System.out.println("Good");
+    //     break;
+    //   case 'C':
+    //     System.out.println("Average");
+    //     break;
+    //   case 'D':
+    //     System.out.println("Below Average");
+    //     break;
+    //   case 'E':
+    //     System.out.println();
+    //     break;
+    //   default:
+    //     System.out.println("제대로 된 성적을 입력하세요.");
+    // }
+
+    // 문제: 변수 operator에 '+', '-', '*', '/' 중 하나의 문자가 저장되어 있습니다. 두 개의 정수형 변수 num1과 num2의 값에 따라 해당 연산을
+    // 수행하고 결과를 출력하는 코드를 작성하세요.
+    // char operator = '-';
+    // int num1 = 22, num2 = 3;
+    //
+    // switch (operator) {
+    //   case '+':
+    //     System.out.println(num1 + num2);
+    //     break;
+    //   case '-':
+    //     System.out.println(num1 - num2);
+    //     break;
+    //   case '*':
+    //     System.out.println(num1 * num2);
+    //     break;
+    //   case '/':
+    //     System.out.println(num1 / num2);
+    //     break;
+    //   default:
+    //     System.out.println("4개의 연산자 중 하나를 선택하세요.");
+    // }
+
+    // 문제: 변수 color에 "red", "green", "blue" 중 하나의 문자열이 저장되어 있습니다. 각 색상에 따라 "Apple", "Grass", "Sky"를
+    // 출력하는 코드를 작성하세요.
+    // String color = "blue";
+    //
+    // switch (color) {
+    //   case "red":
+    //     System.out.println("Apple");
+    //     break;
+    //   case "green":
+    //     System.out.println("Grass");
+    //     break;
+    //   case "blue":
+    //     System.out.println("Sky");
+    //     break;
+    //   default:
+    //     System.out.println("다른 색상을 선택해 주세요.");
+    // }
+
+    // 문제: 변수 score에 0부터 100 사이의 점수가 저장되어 있습니다. 점수 범위에 따라 "A", "B", "C", "D", "F" 등급을 출력하는 코드를
+    // 작성하세요.
+    // int score = 97;
+    //
+    // switch ((int)Math.floor(score/10)) {
+    //   case 9:
+    //     System.out.println('A');
+    //     break;
+    //   case 8:
+    //     System.out.println('B');
+    //     break;
+    //   case 7:
+    //     System.out.println('C');
+    //     break;
+    //   case 6:
+    //     System.out.println('D');
+    //     break;
+    //   default:
+    //     System.out.println('F');
+    // }
+
+    // 문제: 변수 language에 "Java", "Python", "C++", "JavaScript" 중 하나의 문자열이 저장되어 있습니다. 각 프로그래밍 언어에 대한
+    // 간단한 설명을 출력하는 코드를 작성하세요.
+    // String language = "Java";
+    // switch (language) {
+    //   case "Java":
+    //     System.out.println("커피");
+    //     break;
+    //   case "Python":
+    //     System.out.println("뱀");
+    //     break;
+    //   case "C++":
+    //     System.out.println("C++ 어려워");
+    //     break;
+    //   case "JavaScript":
+    //     System.out.println("프론트엔드");
+    //     break;
+    //   default:
+    //     System.out.println("다른 언어를 선택하세요");
+    // }
+
+  }
+}
