68df881e59e9a34d7451797395ce9dd731c5ca8f
Ays <minigun5@naver.com>
2024-06-03 11:41:50 +0900

    [Add] 0603 오전

--- src/OperatorLearn.java
+++ src/OperatorLearn.java
@@ -31,22 +31,72 @@ public class OperatorLearn {
     // System.out.println("전위 감소 연산자: " + (--a));
     // System.out.println("후위 감소 연산자: " + (b--));
 
-    boolean isRainning = true;
+    // boolean isRainning = true;
+    //
+    // if (isRainning) {
+    //   System.out.println("하늘에서 비가 와요.");
+    // }
+    // if(!isRainning) {
+    //   System.out.println("김태희가 안 옵니다.");
+    // }
+    //
+    // boolean isSunny = false;
+    //
+    // if (!isSunny) {
+    //   System.out.println("날씨가 맑아요.");
+    // }
+    // else {
+    //   System.out.println("집에서 쉬어야죠.");
+    // }
 
-    if (isRainning) {
-      System.out.println("하늘에서 비가 와요.");
-    }
-    if(!isRainning) {
-      System.out.println("김태희가 안 옵니다.");
-    }
+    // // 크기 비교
+    // int num1 = 28;
+    // int num2 = 30;
+    //
+    // System.out.println(num1 > num2);
+    // System.out.println(num1 >= num2);
+    // System.out.println(num1 < num2);
+    // System.out.println(num1 <= num2);
+    //
+    // System.out.println(num1 == num2);
+    // System.out.println(num1 != num2);
 
-    boolean isSunny = false;
+    // // 논리 연산: &&(AND), ||(OR), ^(배타적 논리합, XOR)
+    // System.out.println(true && true);
+    // System.out.println(true && false);
+    // System.out.println(false && true);
+    // System.out.println(false && false);
+    //
+    // System.out.println(true || true);
+    // System.out.println(true || false);
+    // System.out.println(false || true);
+    // System.out.println(false || false);
+    //
+    // System.out.println(true ^ true);
+    // System.out.println(true ^ false);
+    // System.out.println(false ^ true);
+    // System.out.println(false ^ false);
 
-    if (!isSunny) {
-      System.out.println("날씨가 맑아요.");
+    // i는 2의 배수이면서 3의 배수
+    // i % 2 == 0 && i % 3 == 0
+
+    // x는 30의 배수이면서 2의 배수이거나 5의 배수이다
+    // x % 30 == 0 && (x % 2 == 0 || x % 5 == 0)
+    // (x % 30 == 0) && ((x % 2 == 0 ) || (x % 5 == 0))
+
+    // // 비트 연산
+    // System.out.println(0b01111 & 0b11110);
+
+    int score = 95;
+    // char grade = (score > 90) ? 'A' : 'B';
+    char grade;
+    // String grade = (score == 95) ? ((score % 5 == 0) ? "good" : "bad") : "bad";
+    if (score > 90) {
+      grade = 'A';
     }
     else {
-      System.out.println("집에서 쉬어야죠.");
+      grade = 'B';
     }
+    System.out.println(grade);
   }
 }
--- src/variable.java
+++ src/variable.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class variable {
   public static void main(String[] args) {
