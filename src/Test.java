ef6c9eaecd558204fe28b675246eee6050cd70aa
Ays <minigun5@naver.com>
2024-06-06 13:53:51 +0900

    [Add] Test 파일 수정

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,10 @@
-public class Test implements TestInterface {
-	// 자바Doc
-	/**
-	@author ys
-	@apiNote  main method
-	**/
-    public static void main(String[] args) {
-		System.out.println("Hello World");
-		// System.out.println("sout");
-	}
-
-	@Override
-	public void test() {
-
-	}
+public class Test {
+  // 자바Doc
+  /**
+   * @author ys
+   * @apiNote main method
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
