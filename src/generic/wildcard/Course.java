30a7d5812f5112398197aa4eb56b8b9972d863d4
Ays <minigun5@naver.com>
2024-06-19 02:38:56 +0900

    [Comment] 코드 보완

--- src/generic/wildcard/Course.java
+++ src/generic/wildcard/Course.java
@@ -6,7 +6,7 @@ public class Course<T> {
 
 	public Course(String name, int capacity) {
 		this.name = name;
-		//타입 파라미터로 배열을 생성하려면 new T[n] 형태로 배열을 생성할 수 없고 (T[]) (new Object[n])으로 생성해야한다.
+		// 타입 파라미터로 배열을 생성하려면 new T[n] 형태로 배열을 생성할 수 없고 (T[]) (new Object[n])으로 생성해야한다.
 		this.students = (T[])(new Object[capacity]);
 	}
 
